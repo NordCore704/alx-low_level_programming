cdfcd6d1b4a9df3cb66ce3f0793965d77b368377
Simon <petersimon0730@gmail.com>
2022-12-10 05:32:12 -0800

    fixing task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,62 +1,36 @@
 #include <stdio.h>
 
-
 /**
-
- * main - prints all possible combinations of two two-digit numbers
-
+ * main -  prints all possible different combinations of three digits.
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
-
 {
+	int ones = '0';
+	int tens = '0';
+	int hundreds = '0';
 
-        int i, j;
-
-
-        for (i = 0; i < 100; i++)
-
-        {
-
-                for (j = 0; j < 100; j++)
-
-                {
-
-                        if (i < j)
-
-                        {
-
-                                putchar((i / 10) + 48);
-
-                                putchar((i % 10) + 48);
-
-                                putchar(' ');
-
-                                putchar((j / 10) + 48);
-
-                                putchar((j % 10) + 48);
-
-                                if (i != 98 || j != 99)
-
-                                {
-
-                                        putchar(',');
-
-                                        putchar(' ');
-
-                                }
-
-                        }
-
-
-                }
-
-        }
-
-        putchar('\n');
-
-        return (0);
-
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+			{
+				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+				{
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9' && hundreds == '7' && tens == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
