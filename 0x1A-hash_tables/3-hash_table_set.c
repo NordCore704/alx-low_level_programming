24a50ccb2e72332946749c0708fd0cb9fe9c2e49
Simon Peter <petersimon0730@gmail.com>
2023-04-07 06:16:35 -0700

    fix 4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,34 +1,34 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Add or update an element in a hash table.
+ * hash_table_set - Add to or update an element in an existing hash table.
  * @ht: A pointer to the hash table.
- * @key: The key to add -this cannot be an empty string.
- * @value: The value in conjuction with key.
+ * @key: The key to be added - this cannot be an empty string.
+ * @value: The value associated with key.
  *
  * Return: On fail - 0.
- *         On success - 1.
+ *         On Success - 1.
  */
-int hash_table_set_func(hash_table_t *ht, const char *key, const char *value)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
-	char *copy_value;
-	unsigned long int index, i;
+	char *value_copy;
+	unsigned long int idx, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	copy_value = strdup(value);
-	if (copy_value == NULL)
+	copy_value= strdup(value);
+	if (copy_value== NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+	idx = key_idx((const unsigned char *)key, ht->size);
+	for (i = idx; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = copy_value;
+			ht->array[i]->value = value_copy;
 			return (1);
 		}
 	}
@@ -36,7 +36,7 @@ int hash_table_set_func(hash_table_t *ht, const char *key, const char *value)
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
-		free(copy_value);
+		free(value_copy);
 		return (0);
 	}
 	new->key = strdup(key);
@@ -45,9 +45,9 @@ int hash_table_set_func(hash_table_t *ht, const char *key, const char *value)
 		free(new);
 		return (0);
 	}
-	new->value = copy_value;
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	new->value = value_copy;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
 
 	return (1);
 }
